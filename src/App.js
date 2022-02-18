19f0eceb9a22e400337d7aabafd6bde37771b132
Naiem YF <iritman@gmail.com>
2022-02-18 20:52:39 +0330

    Implement dark mode

--- src/App.js
+++ src/App.js
@@ -1,8 +1,57 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
+import { Row, Col, Space, Switch } from "antd";
+import { FaMoon as MoonIcon, FaSun as SunIcon } from "react-icons/fa";
 
-function App() {
-  return <div className="app"></div>;
-}
+const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const onDarkModeChange = (checked) => {
+    setIsDarkMode(checked);
+  };
+
+  const darkModeOnHeaderColor = "darkslateblue";
+  const darkModeOnKeyPadColor = "navy";
+  const darkModeOffHeaderColor = "skyblue";
+  const darkModeOffKeyPadColor = "white";
+
+  return (
+    <div className="app">
+      <Row className="app_calculator">
+        <Col
+          xs={24}
+          style={{
+            backgroundColor: isDarkMode
+              ? darkModeOnHeaderColor
+              : darkModeOffHeaderColor,
+            paddingLeft: 10,
+            paddingTop: 10,
+          }}
+        >
+          <Space>
+            <Switch size="small" onChange={onDarkModeChange} />
+
+            {isDarkMode ? (
+              <MoonIcon style={{ color: "white" }} />
+            ) : (
+              <SunIcon style={{ color: "white" }} />
+            )}
+          </Space>
+        </Col>
+        <Col
+          xs={24}
+          style={{
+            backgroundColor: isDarkMode
+              ? darkModeOnKeyPadColor
+              : darkModeOffKeyPadColor,
+          }}
+        >
+          Key pad
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 export default App;
